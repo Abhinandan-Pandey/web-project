ffea9ef136635d3c324f0b6af6e4fdc3027bdaa2
Abhinandan-Pandey <abhinandanpandey.ap@gmail.com>
2020-04-17 19:46:13 +0530

    Updates

---
+++ precache-manifest.1afc40ddbbfd48088e52e255daf07ee1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bedf6df72031dc74bee326009b416c40",
+    "url": "/web-project/index.html"
+  },
+  {
+    "revision": "664be33ed233d14cb99f",
+    "url": "/web-project/static/css/3.73c71b56.chunk.css"
+  },
+  {
+    "revision": "171426cfb2f3e4d4bd79",
+    "url": "/web-project/static/css/main.ee8966fc.chunk.css"
+  },
+  {
+    "revision": "80281c0bf8a663c64dcd",
+    "url": "/web-project/static/js/2.10da4fcb.chunk.js"
+  },
+  {
+    "revision": "664be33ed233d14cb99f",
+    "url": "/web-project/static/js/3.fc377d7e.chunk.js"
+  },
+  {
+    "revision": "0691e89354b93e7be961",
+    "url": "/web-project/static/js/4.2d6f2009.chunk.js"
+  },
+  {
+    "revision": "171426cfb2f3e4d4bd79",
+    "url": "/web-project/static/js/main.d972f9c4.chunk.js"
+  },
+  {
+    "revision": "65f9415787c9e8d11cea",
+    "url": "/web-project/static/js/runtime-main.69f43cb2.js"
+  }
+]);
